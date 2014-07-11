7e9e3a4ab0ab709996377c532c78fbd1b50b36aa
Kurt <kurtcorbett@gmail.com>
2014-07-11 08:31:16 -0600

    First Commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	
+	
+	140 - $('tweet-compose').value()
+	// //Step 1 capture click in text area
+	// $('.tweet-compose').focus(function() {	
+	// //Step 2 toggle the tweet-controls
+	// 	//$('#dashboard').toggle();
+	// 	$('#tweet-controls').toggle();
+	// });
+});
+
+//<textarea class="tweet-compose" placeholder="Compose new Tweet..."></textarea>
+
