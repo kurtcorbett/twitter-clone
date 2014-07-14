2f58b13dfbd7b37df2aaf7dcf23b652bddea58a3
Kurt <kurtcorbett@hotmail.com>
2014-07-14 00:30:59 -0600

    Need to find out how to use 'This' keyword so reply posts don't count down on tweet counter

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
   $('#tweet-controls').hide();
-  
-
+  $('#tweet-submit').attr('disabled',true);
 
   $('.tweet-compose').focus(function(){
-  $(this).height(66);
-  $('#tweet-controls').show();
-  })
+    $(this).height(66);
+    $('#tweet-controls').show();
+  });
 
   $('.tweet-compose').blur(function(){
-  $(this).height(21);
-  })
+    if($(this).val().length === 0) { 
+      $(this).height(21);
+    };
+  });
 
   $('.tweet-compose').keyup(function () {
     var max = 140;
@@ -18,28 +19,95 @@ $(document).ready(function(){
     var maxMinusTen = max - 10; 
     var char = max - len;
 
-  $('#char-count').text(char); 
+    $('#char-count').text(char); 
 
-  if (len > max) {
-    $('#tweet-submit').attr('disabled', true);
-  } else {
-    $('#tweet-submit').attr('disabled',false);
-  }
+    if (len > max) {
+      $('#tweet-submit').attr('disabled', true);
+    } else if (len < 1) {
+      $('#tweet-submit').attr('disabled',true);
+    } else {
+      $('#tweet-submit').attr('disabled',false);
+    }
 
-  if (len >= maxMinusTen) {
-    $('#char-count').css('color','red');
-  } else {
-    $('#char-count').css('color','black')
-  }
+    if (len >= maxMinusTen) {
+      $('#char-count').css('color','red');
+    } else {
+      $('#char-count').css('color','black')
+    }
+
+  });
+
+  var timeStamp = new Date();
+
+
+
+  var user = {
+    'fullName': 'Kurt Corbett',
+    'userName': 'Kcorbett',
+    'img': 'avatar',
+    'imgSrc': 'img/alagoon.jpg'
+  };
+
+
+
+  var addNewTweet = function(user,tweetText) {
+      $('#stream').prepend(
+
+        '<div class="tweet">'+
+            '<div class="content">'+
+              '<img class="' + user.img +'" src="' + user.imgSrc + '" />'+  
+              '<strong class="fullname">' + user.fullName + '</strong> '+
+              '<span class="username">' + user.userName + '</span>'+
+
+              '<p class="tweet-text">' + tweetText + '</p>'+
+
+              '<div class="tweet-actions">'+
+                '<ul>'+
+                  '<li><span class="icon action-reply"></span> Reply</li>'+
+                  '<li><span class="icon action-retweet"></span> Retweet</li>'+
+                  '<li><span class="icon action-favorite"></span> Favorite</li>'+
+                  '<li><span class="icon action-more"></span> More</li>'+
+                '</ul>'+
+              '</div>'+
+
+              '<div class="stats">'+
+                '<div class="retweets">'+
+                  '<p class="num-retweets">30</p>'+
+                  '<p>RETWEETS</p>'+
+                '</div>'+
+                '<div class="favorites">'+
+                  '<p class="num-favorites">6</p>'+
+                  '<p>FAVORITES</p>'+
+                '</div>'+
+                '<div class="users-interact">'+
+                  '<div>'+
+
+                    '<img src="img/alagoon.jpg" />'+
+                    '<img src="img/vklimenko.jpg" />'+
+                  '</div>'+
+                '</div>'+
+
+                '<div class="time">'+
+                  '' + timeStamp + ''+
+                '</div>'+
+              '</div>'+
+              '<div class="reply">'+
+                '<img class="avatar" src="img/alagoon.jpg" />'+
+                '<textarea class="tweet-compose" placeholder="Reply to @mybff"/></textarea>'+
+              '</div>'+
+            '</div>'+
+          '</div>'
+
+    )}
+
+    $('#tweet-submit.button').on('click', function () {
+      var tweetText = $('.tweet-compose').val();
+      addNewTweet(user,tweetText);
+      $('.tweet-compose').val('');
+      $('.tweet-compose').blur();
+    }); 
+
 
 
 
 });
-
-
-
-});
-
-//<textarea class="tweet-compose" placeholder="Compose new Tweet..."></textarea>
-
-//jklfa;jklfsad;jklafsdjklfsadjklfsad;jklfsad;jklfsadjklfads;jklfsad;jklfsad;jklfsad;jklfsadjklfsadjklfsad;jklfsadjkl;fsad;jklfsadj
